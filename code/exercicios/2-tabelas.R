53e7b3a38d5d34a40be37d08d2892583d554d8f4
Rafael Guedes <rafaelguedes.ufam@gmail.com>
2021-10-01 19:33:12 -0400

    Exercicio 2 completo

--- code/exercicios/2-tabelas.R
+++ code/exercicios/2-tabelas.R
@@ -2,26 +2,38 @@
 # tabelas -------------------------------------------------------------
 
 # 1. Use o data.frame airquality para responder às questões abaixo:
-# (a) quantas colunas airquality tem?
+ds <- airquality
 
+# (a) quantas colunas airquality tem?
+ncol(ds)
 
 # (b) quantas linhas airquality tem?
-
+nrow(ds)
 
 # (c) o que a função head() retorna?
-
+    # As 6 primeiras linhas do dataframe
+head(ds)
 
 # (d) quais são os nomes das colunas?
-
+summary(ds)
+    # Ozone, Solar.R, Wind, Tem, Month e Day
 
 # (e) qual é a classe da coluna Ozone? Dicas: class() e '$'
-
+class(ds$Ozone)
+    #integer
 
 # (f) o que o código str(airquality) retorna?
-
+    # O tipo dos dados das colunas no dataset
+str(ds)
 
 # (g) utilizando a resposta da (f), quais são os tipos de
 #     cada coluna da tabela airquality?
+# $ Ozone  : int
+# $ Solar.R: int  
+# $ Wind   : num  
+# $ Temp   : int  
+# $ Month  : int 
+# $ Day    : int 
 
 
 # [desafio] 2. Use o data.frame airquality para responder às questões abaixo:
@@ -31,22 +43,23 @@ airquality <- datasets::airquality # esse pacote faz parte do R base, não é ne
 # Vamos calcular o desvio padrão de Ozone sem usar a função sd().
 # Dica: guarde os resultados de cada item em objetos para poderem ser usados depois.
 # (a) tire a média da coluna Ozone. Dica: use mean(x, na.rm = TRUE).
-
+mediaOz <- mean(ds$Ozone, na.rm = TRUE)
 
 # (b) subtraia da coluna Ozone a sua própria média (centralização).
-
+ozMenosMedia <- ds[!is.na(ds$Ozone),]$Ozone-mediaOz
+    
 
 # (c) eleve o vetor ao quadrado.
-
+ozQuad <- OzMenosMedia^2
 
 # (d) tire a média do vetor.
-
+mediaFinal <- mean(ozQuad)
 
 # (e) tire a raíz quadrada.
-
+raizQ <- sqrt(mediaFinal)
 
 # (f) compare o resultado com sd(airquality$Ozone)
-
+sd(airquality$Ozone, na.rm = TRUE)
 
 
 

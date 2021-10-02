c62eebf6334f9486d81a0382d58705aa9b050b1c
Rafael Guedes <rafaelguedes.ufam@gmail.com>
2021-10-01 20:11:35 -0400

    Exercicio 4 completo

--- code/exercicios/4-operadores.R
+++ code/exercicios/4-operadores.R
@@ -9,16 +9,21 @@ dolar_subiu <- c(TRUE, TRUE, FALSE, FALSE, TRUE, FALSE, TRUE)
 # (ou não subiu - FALSE) no respectivo dia. Interprete os números abaixo:
 
 # (a) length(dolar_subiu)
-
+length(dolar_subiu)
+    # Quantidade de dias
 
 # (b) dolar_subiu[2]  (ps: a semana começa no domingo)
-
+dolar_subiu[2]
+    # O valor do dólar subiu na segunda-feira
 
 # (c) sum(dolar_subiu)
-
+sum(dolar_subiu)
+    # Quantidade de dias em que o dólar subiu
 
 # (d) mean(dolar_subiu)
-
+mean(dolar_subiu)
+# Porcentagem dos dias da semana em que o dólar subiu
 
 # (e) Agora observe a saída de as.numeric(dolar_subiu). O que o R fez?
-
+as.numeric(dolar_subiu)
+    # Converteu TRUE para 1 e FALSE para 0
